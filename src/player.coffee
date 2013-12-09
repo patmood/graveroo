ab664cc068ac983ff138a9f67945103eac92ded4
patmood <patrick.n.moody@gmail.com>
2013-12-09 14:00:39 +1030

    player class

---
+++ src/player.coffee
@@ -0,0 +1,5 @@
+class Player
+  x: 0
+  y: 0
+  w: 18
+  h: 24
--- watch.sh
+++ watch.sh
@@ -2,5 +2,6 @@
 
 xargs coffee -w -b -j script/main.js \
              -c << EOF
+                src/player.coffee
                 src/gfx.coffee
                 src/game.coffee
